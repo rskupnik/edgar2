9d30f7c12665829bf30276c709f6ef0c4a4e91c7
Radoslaw Skupnik <r.skupnik@gmail.com>
2021-01-18 17:37:21 +0100

    Some CSS experiments

--- http/src/main/java/com/github/rskupnik/edgar2/web/DashboardController.java
+++ http/src/main/java/com/github/rskupnik/edgar2/web/DashboardController.java
@@ -1,8 +1,10 @@
 package com.github.rskupnik.edgar2.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class DashboardController {
 
--- http/src/main/java/com/github/rskupnik/edgar2/web/DeviceController.java
+++ http/src/main/java/com/github/rskupnik/edgar2/web/DeviceController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class DeviceController {
 
